842dbea61ad19626885869ac95675e81ce76e31f
user <jakub.razek2@gmail.com>
2022-08-07 22:38:53 +0200

    vulkan-headers

---
+++ heat_diffusion/include/heat_diffusion.hpp
@@ -0,0 +1,4 @@
+
+namespace jr::math::simulations::heat_diffusion{
+	struct HeatDiffusion{};
+}
--- heat_diffusion/src/main.cpp
+++ heat_diffusion/src/main.cpp
@@ -1,6 +1,14 @@
+
 #include <iostream>
 #include <glfwpp/glfwpp.h>
+#include <vulkan/vulkan.hpp>
+
+
 
 auto main() -> int{
 	auto glfw_instance = glfw::init();
+	glfw::Window window{ 640, 480, "Hello GLFWPP"};
+
+	while (!window.shouldClose())
+        glfw::pollEvents();
 }
